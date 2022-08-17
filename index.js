c575a23e88fe3f61b0f148cc0ad5ba119802e930
Emmanuel Odhiambo <emmanuel3odhiambo@gmail.com>
2022-08-17 23:23:44 +0300

    string review complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = "Emmanuel";  //defines currentUser
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + (currentUser) + '!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; //defines welcomeMessage + string concatenation
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` //using slice method to return only first letter of currentUser
+
+
+
+
